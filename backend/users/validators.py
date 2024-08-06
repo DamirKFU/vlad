d551ea75d92c8fa76559e3e6172cd43ca1f4de66
DamirKFU <shabievdg@yandexlyceum.ru>
2024-08-06 11:43:29 +0300

    refact: username validation

--- backend/users/validators.py
+++ backend/users/validators.py
@@ -5,6 +5,6 @@ import django.core.validators
 
 @django.utils.deconstruct.deconstructible
 class UsernameValidator(django.core.validators.RegexValidator):
-    regex = r"^[\w_]{5,32}"
+    regex = r"^(?=.{5,32}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$"
     message = "username_validation_error"
     flags = re.ASCII
