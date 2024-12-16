bab3dca1c6c58663d7419df829984842b6a0f6d1
Damir-KFU <sahbievdg@yandexlyceum.ru>
2024-12-16 20:25:56 +0300

    feat: update username validation message to include password requirements
    - Changed the username validation message to provide detailed password requirements in Russian, enhancing user guidance during account creation.

--- backend/users/validators.py
+++ backend/users/validators.py
@@ -7,7 +7,14 @@ import django.core.validators
 @django.utils.deconstruct.deconstructible
 class UsernameValidator(django.core.validators.RegexValidator):
     regex = r"^(?=.{5,32}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$"
-    message = "username_validation_error"
+    message = (
+        "Пароль должен соответствовать следующим требованиям:\n"
+        "- Длина от 8 до 128 символов\n"
+        "- Минимум одна прописная буква\n"
+        "- Минимум одна строчная буква\n"
+        "- Минимум одна цифра\n"
+        "- Без пробелов"
+    )
     flags = re.ASCII
 
 
