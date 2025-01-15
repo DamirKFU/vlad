9101be371cf0f1775a7b55ef913491a5206cbfd1
DamirKFU <sahbievdg@yandexlyceum.ru>
2025-01-16 01:39:07 +0300

    chore: update database settings for improved environment variable defaults
    - Modified database configuration in settings.py to include default values for PostgreSQL environment variables, enhancing local development setup.
    - This change allows for easier configuration without requiring all environment variables to be explicitly set, improving flexibility for developers.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,11 +74,11 @@ WSGI_APPLICATION = "backend.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("POSTGRES_HOST"),
-        "PORT": os.getenv("POSTGRES_PORT"),
+        "NAME": os.getenv("POSTGRES_DB", "myproject"),
+        "USER": os.getenv("POSTGRES_USER", "myprojectuser"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "password"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRES_PORT", ""),
     }
 }
 
